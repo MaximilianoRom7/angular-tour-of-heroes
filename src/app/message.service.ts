5d645fb23e07743aaac78781f101a20b1bf4f436
Maximiliano Romay <maximilianoromayfigueroa@hotmail.com>
2018-04-21 20:19:57 -0300

    the messages on the messages component are filtered and reversed from all the messages in the messages service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,14 +3,29 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MessageService {
 
+    messages: string[] = [];
+    onAddHandlers = [];
+
     constructor() {
         this.messages = [];
     }
 
-    messages: string[];
-
     add(message: string): void {
         this.messages.push(message);
+        this.onAddHandlersCall();
+    }
+
+    /* trigger: when add method is called */
+    onAdd(func): void {
+        if(this.onAddHandlers.indexOf(func) > -1)
+            /* handler alredy registered */
+            return
+        this.onAddHandlers.push(func);
+    }
+
+    /* call all the onAdd handlers */
+    onAddHandlersCall(): void {
+        this.onAddHandlers.map(e => e());
     }
 
     clear(): void {
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -8,8 +8,30 @@ import { MessageService } from '../message.service';
 })
 export class MessagesComponent implements OnInit {
 
+    /*
+      messages filtered from all the messages
+      at messagesService.messages
+    */
+    messages_filtered: string[];
+
+    /* max amount of messages to show in the view */
+    messages_max: number = 20;
+
     constructor(public messageService: MessageService) { }
 
-    ngOnInit() { }
+    filterMessages(): void {
+        var filter = this.messageService.messages
+            .slice(-this.messages_max)
+            .reverse();
+        this.messages_filtered = filter;
+    }
+
+    bindFilterMessages(): void {
+        this.messageService.onAdd((this.filterMessages).bind(this));
+    }
+
+    ngOnInit() {
+        this.bindFilterMessages();
+    }
 
 }
