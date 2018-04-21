e8486a74b752c0be435f32d76899d19217813d78
Maximiliano Romay <maximilianoromayfigueroa@hotmail.com>
2018-04-21 19:26:26 -0300

    the message component now shows the messages from the message service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,17 +4,19 @@ import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-    constructor() { }
+    constructor(private messageService: MessageService) { }
 
     /*
       This method returns an observable the same
       way the http lib does, making this request async
      */
     getHeroes(): Observable<Hero[]> {
+        this.messageService.add("HeroService: fetched heroes");
         return of(HEROES);
     }
 
